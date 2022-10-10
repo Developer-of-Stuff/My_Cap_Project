12f3a2f0b083d6d405dfc8121cc5d9c651c76b95
Developer-of-Stuff <agulley@protonmail.com>
2022-10-09 23:47:02 -0400

    Changed Procfile and added gunicorn

--- venv/main.py
+++ venv/main.py
@@ -4,5 +4,4 @@ from website.data_loading import MatrixFactorization, Loader
 app = create_app()
 
 if __name__ == '__main__':
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    app.run()
